c31660d1c2bb175511fba3d858c9201785f5a1a9
Pavel <pavlogenizoy@gmail.com>
2021-03-26 18:56:19 +0200

    added last 2 criterion and some fixed# 4

--- Lab_2/main_task.py
+++ Lab_2/main_task.py
@@ -8,7 +8,6 @@ def expcdf(t, lyambd=1):
 
 def kolmogorov(lyambda, size):
     sample = np.sort(np.random.exponential(scale=1/lyambda, size=size))
-    # sample = np.sort(sample)
 
     k_it = np.array([x for x in range(1, size + 1)])
 
@@ -52,12 +51,55 @@ def chi_square(lyambda, size):
 ##############################################################################################################
 
 
+def critical_limit(r):
+    return r * np.exp(-2) + stats.norm.ppf(1 - gamma) * np.sqrt(r * np.exp(-2) * (1 - 3 * np.exp(-2)))
+
+
+def empty_boxes(lyambda, size):
+    sample_exp_cdf = expcdf(np.random.exponential(scale=1 / lyambda, size=size))
+
+    r_interval = int(size / 2)
+    count_zero_boxes = v_frequencies(sample_exp_cdf, r_interval).count(0)
+    t_alpha = critical_limit(r_interval)
+
+    if count_zero_boxes < t_alpha:
+        print(f"\t\tcount_zero_boxes = {count_zero_boxes}; critical_limit = {t_alpha} || Statistics do not "
+              f"contradict the hypothesis H_0", file=file)
+    else:
+        print(f"\t\tcount_zero_boxes = {count_zero_boxes}; critical_limit = {t_alpha} || An alternative "
+              f"hypothesis should be accepted H_1", file=file)
+
+
+##############################################################################################################
+
+
+def smirnov(lyambda, size):
+    sample_n = np.sort(np.random.exponential(scale=1, size=size))
+    m_size = int(size / 2)
+    sample_m = np.sort(np.random.exponential(scale=1 / lyambda, size=m_size))
+
+    k_it = np.array([x for x in range(1, m_size + 1)])
+
+    d_value = (np.maximum((k_it / m_size) - expcdf(sample_m), expcdf(sample_m) - (k_it - 1 / m_size))).max()
+    critical_val = z * np.sqrt((1 / size) + (1 / m_size))
+
+    if d_value <= critical_val:
+        print(f"\t\tD = {d_value}; critical_val = {critical_val} || Statistics do not contradict the hypothesis H_0",
+              file=file)
+    else:
+        print(f"\t\tD = {d_value}; critical_val = {critical_val}|| An alternative hypothesis should be accepted H_1",
+              file=file)
+
+
+##############################################################################################################
+
+
 if __name__ == '__main__':
     z = 1.36  # at gamma = 0.05
     gamma = 0.05
-    dict_for_func = {0: kolmogorov, 1: chi_square}
+    dict_for_func = {0: kolmogorov, 1: chi_square, 2: empty_boxes, 3: smirnov}
 
-    for it, name in enumerate(['kolmogorov', 'chi_square']):
+    for it, name in enumerate(['kolmogorov', 'chi_square', 'empty_boxes', 'smirnov']):
         with open("Results\\" + name + "_result.txt", "w") as file:
             for lyambda in [1, 1.3]:
                 print(f"H_0: X_i belongs to F(u, 1), when real is X_i belongs to F(u, {lyambda})", file=file)
